273f6891f6b46df17fce5afdb289cd355a8019e8
Dishebh Bhayana <dishebh27@gmail.com>
2020-06-14 16:29:29 +0530

    added middlewares in api

---
+++ middleware/checkObjectId.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// Middleware to check for a valid Object Id
+const checkObjectId = (idToCheck) => (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params[idToCheck])) {
+    return res.status(400).json({ msg: "Invalid ID!" });
+  }
+
+  next();
+};
+
+module.exports = checkObjectId;
